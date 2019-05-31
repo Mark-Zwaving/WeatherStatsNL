0221669d0587cfcdc0495679785a8bb0b47f205f
Mark Zwaving <markzwaving@gmail.com>
2019-05-31 13:43:32 +0200

    improvements in code

--- sources/calc_stats.py
+++ sources/calc_stats.py
@@ -5,11 +5,11 @@ __author__     =  "Mark Zwaving"
 __email__      =  "markzwaving@gmail.com"
 __copyright__  =  "Copyright 2019 (C) Mark Zwaving. All rights reserved."
 __license__    =  "GNU Lesser General Public License (LGPL)"
-__version__    =  "0.9"
+__version__    =  "0.9.1"
 __maintainer__ =  "Mark Zwaving"
 __status__     =  "Development"
 
-import config as c, knmi
+import config as c, knmi, ask as a
 
 class EtmgegGem():
     '''Klasse berekent gemiddelde waarden bestaande uit som en aantal in een reeks van dagen'''
@@ -42,28 +42,58 @@ class EtmgegCount():
 
 class EtmgegSom():
     '''Klasse voor het opslaan van som extreem'''
-    def __init__(self, datum, tijd, waarde, som, tel, ent ):
+    def __init__(self, datum, tijd, waarde, som, ent ):
         self.datum   = datum
         self.tijd    = tijd
         self.waarde  = waarde
         self.som     = som
-        self.tel     = tel
         self.ent     = ent
+
+class WarmteGetal:
+    '''klasse voor het opslaan van warmtegetal dagen'''
+    def __init__(self, datum, tg, getal, totaal, aantal, ent):
+        self.datum  = datum
+        self.tg     = tg
+        self.getal  = getal
+        self.totaal = totaal
+        self.aantal = aantal
+        self.ent    = ent
+
+class Hellmann:
+    '''Klasse voor het opslaan van hellman dagen'''
+    def __init__(self, datum, getal, som, aantal, ent):
+        self.datum  = datum
+        self.getal  = getal
+        self.som    = som
+        self.aantal = aantal
+        self.ent    = ent
+
+class Heatwave:
+    '''Klasse voor het opslaan van hittegolf dagen'''
+    def __init__(self, station, etmgeg_list ):
+        self.station      = station
+        self.etmgeg_list  = etmgeg_list
+        self.tot_heat_sum = warmte_getal(self.etmgeg_list)['getal']
+        self.day_count    = len(self.etmgeg_list)
 
 # Som
 def som_val(lijst_geg, ent):
     '''Functie somt alle waarden van een reeks'''
     som, tel, tijd, l = 0, 0, -1, []
     for geg in lijst_geg: # Doorloop lijst
-        etm = knmi.etmgeg(geg,ent)
+        etm = knmi.etmgeg(geg, ent)
         if etm != geg.empthy: # Leeg veld skippen
-            i_etm = int(etm); som += i_etm; tel += 1 # Convert to int en telop
-            etm_t = knmi.etmgeg_t(geg,ent) # Check tijd
+            i_etm = int(etm)
+            som += i_etm
+            etm_t = knmi.etmgeg_t( geg, ent ) # Check tijd
             tijd = etm_t if etm_t != geg.empthy and etm_t != False else -1 # Geef tijd mits aanwezig
-            l.append(EtmgegSom(geg.YYYYMMDD, tijd, i_etm, som, tel, ent)) # Bewaar som in dag object
-    # Return false als niks is gevonden anders geef waarde met tijdstip lus de lijst met gevonden extremen
-    return { 'som': False, 'lijst':l} if not l \
-             else {'som':l[-1].som,'lijst':l }
+
+            etmSom = EtmgegSom(geg.YYYYMMDD, tijd, i_etm, som, ent)
+            l.append(etmSom) # Bewaar som in som object
+
+    # Return false als niks wordt gevonden anders geef som
+    return { 'som': False if not l else l[-1].som,
+             'lijst':l }
 
 # Gemiddelden
 def gem_val ( lijst_geg, ent ):
@@ -72,12 +102,16 @@ def gem_val ( lijst_geg, ent ):
     for geg in lijst_geg: # Doorloop de lijst
         etm = knmi.etmgeg(geg,ent)
         if etm != geg.empthy: # Leeg veld overslaan
-            datum = geg.YYYYMMDD; i_etm = int(etm); som += i_etm; tel += 1 # verwerk gegevens
-            if c.log: print('GEM|datum:{0}|i_etm:{1}|som:{2}|tel:{3}'.format(datum,i_etm,som,tel))
-            l.append(EtmgegGem(datum, i_etm, som, tel, ent))
-    # Return false als geen waarden zijn gevonden anders geef het gemiddelde en de lijst met waarden
-    return { 'gem':False, 'lijst':l } if not l \
-              else { 'gem':l[-1].gem, 'lijst': l }
+            i_etm = int(etm)
+            som += i_etm
+            tel += 1
+
+            etmGem = EtmgegGem(geg.YYYYMMDD, i_etm, som, tel, ent)
+            l.append(etmGem)
+
+    # Return false als niks wordt gevonden
+    return { 'gem': False if not l else l[-1].gem,
+             'lijst': l }
 
 # Minimum extremen
 def min_val(lijst_geg, ent):
@@ -88,12 +122,16 @@ def min_val(lijst_geg, ent):
         if etm != geg.empthy: # Leeg veld skippen
             i_etm = int(etm) # Convert naar int
             if i_etm < min: # Waarde kleiner dan min dan een nieuw min
-                min  = i_etm; etm_t = knmi.etmgeg_t(geg,ent)
+                min  = i_etm;
+                etm_t = knmi.etmgeg_t( geg, ent )
                 tijd = etm_t if etm_t != geg.empthy and etm_t != False else -1 # Geef tijd mits aanwezig
-                l.append(EtmgegExtreem(geg.YYYYMMDD,tijd,min,ent)) # Bewaar extreem in dag object
-    # Return false als niks is gevonden anders geef waarde met tijdstip lus de lijst met gevonden extremen
-    return { 'min':False,'tijd':0,'lijst':l} if not l \
-             else {'min':l[-1].extreem,'tijd':l[-1].tijd,'lijst':l}
+
+                etmExtreem = EtmgegExtreem( geg.YYYYMMDD, tijd, min, ent )
+                l.append(etmExtreem)
+
+    # Return false als niks is gevonden
+    return { 'min': False if not l else l[-1].extreem,
+             'lijst': l }
 
 # Maximum extremen
 def max_val(lijst_geg, ent):
@@ -104,50 +142,172 @@ def max_val(lijst_geg, ent):
         if etm != geg.empthy: # Leeg veld skippen
             i_etm = int(etm) # Convert naar int
             if i_etm > max: # Waarde groter dan max, dan een nieuw max
-                max  = i_etm; etm_t = knmi.etmgeg_t(geg,ent) # Bepaal nieuwe waarden
+                max  = i_etm;
+                etm_t = knmi.etmgeg_t( geg, ent ) # Bepaal nieuwe waarden
+
                 tijd = etm_t if etm_t != geg.empthy and etm_t != False else -1 # Geef tijd mits aanwezig
-                l.append(EtmgegExtreem(geg.YYYYMMDD,tijd,max,ent)) # Bewaar extreem in dag object
-    # Return false als niks is gevonden anders geef de gevonden waarde met eventuele tijdstip
-    # Plus de hele lijst met gevonden extremen
-    return { 'max':False,'tijd':0,'lijst':l} if not l \
-             else {'max':l[-1].extreem,'tijd':l[-1].tijd,'lijst':l}
+
+                etmExtreem = EtmgegExtreem( geg.YYYYMMDD, tijd, max, ent )
+                l.append(etmExtreem)
+
+    # Return false als niks is gevonden
+    return { 'max': False if max == c.min_value_geg else l[-1].extreem,
+             'lijst': l }
 
 # Counter voorwaardelijk
 def cnt_day(lijst_geg, ent, oper, val):
     '''Functie bepaalt het aantal dagen onder de gestelde voorwaarden'''
-    tel, tijd, l = 0, -1, [] # Hoogst mogelijke waarde
+    tel, tijd, l = 0, -1, []
 
     for geg in lijst_geg: # Doorloop lijst
-        etm = knmi.etmgeg(geg,ent)
+        etm = knmi.etmgeg( geg, ent )
         if etm != geg.empthy: # Leeg veld skippen
-            i_etm, oke = int(etm), True
+            i_etm = int(etm)
 
-            if     oper == '<'  and i_etm <  val: tel += 1
-            elif ( oper == '<=' or oper == '≤' ) and i_etm <= val: tel += 1
-            elif   oper == '>'  and i_etm >  val: tel += 1
-            elif ( oper == '>=' or oper == '≥' ) and i_etm >= val: tel += 1
-            elif ( oper == '<>' or oper == '!=' or oper == 'not' ) and i_etm is not val: tel += 1
-            elif ( oper == '==' or oper == 'is') and i_etm == val: tel += 1
+            oke = True
+            lt = ( oper == '<'  or oper == 'lt' )
+            le = ( oper == '<=' or oper ==  '≤' or oper ==  'le' )
+            gt = ( oper == '>'  or oper == 'gt' )
+            ge = ( oper == '>=' or oper ==  '≥' or oper ==  'ge' )
+            no = ( oper == '<>' or oper == '!=' or oper == 'not' )
+            eq = ( oper == '==' or oper == 'is')
+            if    lt and i_etm <  val: tel += 1
+            elif  le and i_etm <= val: tel += 1
+            elif  gt and i_etm >  val: tel += 1
+            elif  ge and i_etm >= val: tel += 1
+            elif  no and i_etm != val: tel += 1
+            elif  eq and i_etm == val: tel += 1
             else: oke = False
 
-            if c.log:
-                print("CNT|oke:{0}|getal:{1}|oper:{2}|val:{3}|tel:{4}|ent:{5}".format(oke,i_etm,oper,val,tel,ent))
-
             if oke:
-                datum = geg.YYYYMMDD; etm_t = knmi.etmgeg_t(geg,ent) # Bepaal nieuwe waarden
-                tijd = etm_t.strip() if etm_t != geg.empthy and etm_t != False else -1 # Geef tijd mits aanwezig
-                if c.log: print("OKE|tijd:{0}".format(tijd))
-                l.append(EtmgegCount(datum, tijd, i_etm, oper, val, tel, ent)) # Bewaar in count object
+                etm_t = knmi.etmgeg_t( geg, ent ) # Bepaal nieuwe waarden
+                tijd = etm_t if etm_t != geg.empthy and etm_t != False else -1 # Geef tijd mits aanwezig
+
+                etmCount = EtmgegCount(geg.YYYYMMDD, tijd, i_etm, oper, val, tel, ent)
+                l.append(etmCount)
 
     # Return tel en geef de lijst met de extremen
-    return { 'tel':0,'tijd':tijd,'lijst':l} if not l \
-             else {'tel':l[-1].tel,'tijd':l[-1].tijd,'lijst':l}
+    return { 'tel': tel, 'lijst': l }
+
+def hellmann_getal(lijst_geg):
+    som, aantal, l = 0, 0, []
+    for geg in lijst_geg:
+        if geg.TG != geg.empthy:
+            iTG = int(geg.TG)
+            if iTG < 0:
+                getal = abs(iTG)
+                som += getal
+                aantal += 1
+
+                hellman = Hellmann(geg.YYYYMMDD, getal, som, aantal, 'TG')
+                l.append(hellman)
+
+    return { 'getal': som, 'lijst': l }
+
+def warmte_getal(lijst_geg):
+    som, aantal, l, cummula = 0, 0, [], []
+    for geg in lijst_geg:
+        if geg.TG != geg.empthy:
+            iTG = int(geg.TG)
+            if iTG >= 180:
+                getal = iTG - 180
+                som += getal
+                aantal += 1
+
+                warm = WarmteGetal(geg.YYYYMMDD, iTG, getal, som, aantal, 'TG')
+                l.append( warm )
+
+    return { 'getal': som, 'lijst': l }
 
 def get_heat_ndx_of_etm_geg(etm_geg):
     getal = 0
     if etm_geg.TG != etm_geg.empthy:
         iTG = int(etm_geg.TG)
         if iTG >= 180:
-            getal = iTG - 180
+            return iTG - 180
 
-    return getal
+def rearrange_heat_sum_for_period(l):
+
+    pass
+
+def get_list_heatwaves( station, etmgeg_list ):
+    heatwave_list, mem_etmgeg_list = [], []
+    day_cnt_num = 5
+    day_cnt_25  = 0
+    day_cnt_30  = 0
+    day_cnt_30_1 = False
+    day_cnt_30_2 = False
+    day_cnt_30_3 = False
+    heatwave = False
+
+    for e in etmgeg_list:
+        if e.TX != e.empthy:
+            iTX = int(e.TX)
+
+            if iTX >= 250: # Check only 25+
+                day_cnt_25 += 1 # Count 25+
+                etmgeg_list.append(e) # Save 25+
+                if c.debug: a.pause(f'Counting for heatwave: {station.plaats}')
+
+                if not heatwave: # No heatwave yet, check for it
+                    # Check for 3 times 30+
+                    if iTX >= 300:
+                        day_cnt_30 += 1
+                        if c.debug: a.pause('30+ found')
+
+                        if not day_cnt_30_1 and day_cnt_30 == 1:
+                            day_cnt_30_1 = True  # First 30 true
+                            if c.debug: a.pause('First 30+ found')
+
+                        if not day_cnt_30_2 and day_cnt_30 == 2:
+                            day_cnt_30_2 = True  # Second 30 true
+                            if c.debug: a.pause('Second 30+ found')
+
+                        if not day_cnt_30_3 and day_cnt_30 == 3:
+                            day_cnt_30_3 = True  # Third 30 truez
+                            if c.debug: a.pause('Third 30+ found')
+
+                    if day_cnt_30_3 and day_cnt_25 >= day_cnt_num:
+                        heatwave = True # Heatwave started or active
+                        if c.debug: a.pause('Heatwave started')
+            else: # <25
+                if c.debug: a.pause('Counting stopped')
+                if heatwave == True: # Heatwave
+                    heatwave = False
+                    if c.debug: a.pause('Heatwave stopped')
+                    # Add closed heatwave to list
+                    heatdays = Heatwave( station, etmgeg_list )
+                    heatwave_list.append( heatdays )
+                else:
+                    pass # No heatwave ended, just pass
+
+                # No 25+, reset heatwave values
+                day_cnt_25 = 0
+                day_cnt_30 = 0
+                day_cnt_30_1 = False
+                day_cnt_30_2 = False
+                day_cnt_30_3 = False
+                heatwave = False
+                etmgeg_list = []
+
+        else: # Data cannot be checked.
+            if c.debug: a.pause('Wrong data')
+            if heatwave: # Oke stop heatwave, add to list and reset values
+                heatdays = Heatwave( station, etmgeg_list )
+                heatwave_list.append( heatdays )
+
+            # No 25+, reset heatwave values
+            day_cnt_25 = 0
+            day_cnt_30 = 0
+            day_cnt_30_1 = False
+            day_cnt_30_2 = False
+            day_cnt_30_3 = False
+            heatwave = False
+            etmgeg_list = []
+
+    # No more data
+    if heatwave: # Add heatwave to list, heatwave is not ended yet
+        heatdays = Heatwave( station, etmgeg_list )
+        heatwave_list.append ( heatdays )
+
+    return heatwave_list
