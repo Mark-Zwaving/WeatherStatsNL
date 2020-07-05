3716a442ee5c049238f166fb32e7afffda0fa833
mzwaving <markzwaving@gmail.com>
2020-07-05 18:15:27 +0200

    Removed -1 for rh, rhx and sq option
    Over here the -1 option is not needed anymore. Because now it wil be take care will reading/selecting  the data in the period

--- sources/knmi/view/fix.py
+++ sources/knmi/view/fix.py
@@ -5,134 +5,226 @@ __author__     =  "Mark Zwaving"
 __email__      =  "markzwaving@gmail.com"
 __copyright__  =  "Copyright 2020 (C) Mark Zwaving. All rights reserved."
 __license__    =  "GNU Lesser General Public License (LGPL)"
-__version__    =  "0.4"
+__version__    =  "0.0.7"
 __maintainer__ =  "Mark Zwaving"
 __status__     =  "Development"
 
+import config
 import view.translate as tr
 import model.convert as cvt
 
 def value(val, entity):
-    ent = entity.strip().lower()
+    '''Function turns data from etmgeg into the real values'''
+    f = float(val)
+    e = entity.strip().lower()
+
     # Indexes
-    if ent in ['heat_ndx', 'hellmann']: return round(val / 10.0, 1)
+    if e in ['heat_ndx', 'hellmann']:
+        return f / 10.0
+
     # Temperatures
-    elif ent in [ 'tx', 'tn', 'tg', 't10n' ]: return round( float(val) / 10.0, 1 )
+    elif e in [ 'tx', 'tn', 'tg', 't10n' ]:
+        return f / 10.0
+
     # Airpressure
-    elif ent in [ 'pg', 'pn', 'px' ]: return round( float(val) / 10.0 )
+    elif e in [ 'pg', 'pn', 'px' ]:
+        return f / 10.0
+
     # Radiation
-    elif ent in [ 'q' ]: return round( float(val) / 10.0, 1 )
+    elif e in [ 'q' ]:
+        return f / 10.0
+
     # Percentages
-    elif ent in [ 'ug', 'ux', 'un', 'sp' ]: return round(val)
+    elif e in [ 'ug', 'ux', 'un', 'sp' ]:
+        return f
+
     # Time hours
-    elif ent in [ 'fhxh', 'fhnh', 'fxxh', 'tnh', 'txh', 'rhxh',
-                  'pxh', 'vvnh', 'vvxh', 'uxh', 'unh', 'pnh' ]: return round(val)
+    elif e in [ 'fhxh', 'fhnh', 'fxxh', 'tnh', 'txh', 'rhxh',
+                'pxh', 'vvnh', 'vvxh', 'uxh', 'unh', 'pnh' ]:
+        return f
+
     # Time 6 hours
-    elif ent in [ 't10nh' ]: return round(val)
+    elif e in [ 't10nh' ]:
+        return f
+
     # CLouds cover/octants
-    elif ent in [ 'ng' ]: return round(val)
+    elif e in [ 'ng' ]:
+        return f
+
     # Wind
-    elif ent in [ 'fhvec','fg','fhx','fhn','fxx' ]: return round( float(val) / 10.0, 1 )
+    elif e in [ 'fhvec','fg','fhx','fhn','fxx' ]:
+        return f / 10.0
+
     # Evapotranspiration
-    elif ent in [ 'ev24', 'rh', 'rhx' ]:
-        if val == -1:
-        	return 0.05
-        else:
-            return round( float(val) / 10.0, 1)
+    elif e in [ 'ev24', 'rh', 'rhx' ]:
+        return f / 10.0
+
     # Duration hours
-    elif ent in [ 'sq', 'dr' ]:
-        if val == -1:
-            return 0.05
-        else:
-            return round( float(val) / 10.0, 1)
+    elif e in [ 'sq', 'dr' ]:
+        return f / 10.0
 
     # Wind direction
-    elif ent in  [ 'ddvec' ]: return round(val)
-    # View distance
-    elif ent in [ 'vvn', 'vvx' ]: return round(val)
+    elif e in  [ 'ddvec' ]:
+        return f
 
-    return round(val)
+    # View distance
+    elif e in [ 'vvn', 'vvx' ]:
+        return f
+
+    return f  # Happens for dummy values
+
+def rounding(val, entity):
+    '''Function turns data from etmgeg into the real values'''
+    e = entity.strip().lower()
+    f = value(val, e)
+
+    # Indexes
+    if e in ['heat_ndx', 'hellmann']:
+        return round(f)
+
+    # Temperatures
+    elif e in [ 'tx', 'tn', 'tg', 't10n' ]:
+        return round(f,1)
+
+    # Airpressure
+    elif e in [ 'pg', 'pn', 'px' ]:
+        return round(f,1)
+
+    # Radiation
+    elif e in [ 'q' ]:
+        return round(f,1)
+
+    # Percentages
+    elif e in [ 'ug', 'ux', 'un', 'sp' ]:
+        return round(f)
+
+    # Time hours
+    elif e in [ 'fhxh', 'fhnh', 'fxxh', 'tnh', 'txh', 'rhxh',
+                'pxh', 'vvnh', 'vvxh', 'uxh', 'unh', 'pnh' ]:
+        return round(f)
+
+    # Time 6 hours
+    elif e in [ 't10nh' ]:
+        return round(f)
+
+    # CLouds cover/octants
+    elif e in [ 'ng' ]:
+        return round(f)
+
+    # Wind
+    elif e in [ 'fhvec','fg','fhx','fhn','fxx' ]:
+        return round(f,1)
+
+    # Evapotranspiration
+    elif e in [ 'ev24', 'rh', 'rhx' ]:
+        return round(f,1)
+
+    # Duration hours
+    elif e in [ 'sq', 'dr' ]:
+        return round(f,1)
+
+    # Wind direction
+    elif e in  [ 'ddvec' ]:
+        return round(f)
+
+    # View distance
+    elif e in [ 'vvn', 'vvx' ]:
+        return round(f,1)
+
+    return f  # Happens for dummy values
 
 def ent(val, entity):
     '''Function adds correct post/prefixes for weather entities'''
-    ent = entity.strip().lower()
-    val = value(val, entity)
+    e = entity.strip().lower()
+    f = value(val, e)
+
+    # No measurement or false measurement
+    if f == config.knmi_dayvalues_dummy_val:
+        return '...'
 
     # Indexes
-    if ent in ['heat_ndx', 'hellmann']:
-        return str(val)
+    elif e in ['heat_ndx', 'hellmann']:
+        return f'{f:.0f}'
 
     # Temperatures
-    elif ent in [ 'tx', 'tn', 'tg', 't10n' ]:
-        return f'{val}°C'
+    elif e in [ 'tx', 'tn', 'tg', 't10n' ]:
+        return f'{f:.1f}°C'
 
     # Airpressure
-    elif ent in [ 'pg', 'pn', 'px' ]:
-        return f'{val}hPa'
+    elif e in [ 'pg', 'pn', 'px' ]:
+        return f'{f:.0f}hPa'
 
     # Radiation
-    elif ent in [ 'q' ]:
-        return f'{val}J/cm2'
+    elif e in [ 'q' ]:
+        return f'{f:.1f}J/cm2'
 
     # Percentages
-    elif ent in [ 'ug', 'ux', 'un', 'sp' ]:
-        return f'{val}%'
+    elif e in [ 'ug', 'ux', 'un', 'sp' ]:
+        return f'{f:.0f}%'
 
     # Time hours
-    elif ent in [ 'fhxh', 'fhnh', 'fxxh', 'tnh', 'txh', 'rhxh',
+    elif e in [ 'fhxh', 'fhnh', 'fxxh', 'tnh', 'txh', 'rhxh',
                   'pxh', 'vvnh', 'vvxh', 'uxh', 'unh', 'pnh' ]:
-        return f'{val-1}-{val} {tr.txt("hour")}'
+        f1 = f'{f:.0f}'
+        f2 = f'{(f-1):.0f}'
+        return f'{f2}-{f1} {tr.txt("hour")}'
 
     # Time 6 hours
-    elif ent in [ 't10nh' ]:
-        return f'{val-6}-{val} {tr.txt("UT")}'
+    elif e in [ 't10nh' ]:
+        f1 = f'{f:.0f}'
+        f2 = f'{(f-6):.0f}'
+        return f'{f2}-{f1} {tr.txt("hour")}'
 
     # CLouds cover/octants
-    elif ent in [ 'ng' ]:
-        return str(val)
+    elif e in [ 'ng' ]:
+        return f'{f:.0f}'
 
     # Wind
-    elif ent in [ 'fhvec','fg','fhx','fhn','fxx' ]:
+    elif e in [ 'fhvec','fg','fhx','fhn','fxx' ]:
         bft = cvt.ms_to_bft(val)
-        return f'{val}m/s {bft}bft'
+        return f'{f:.1f}m/s {bft}bft'
 
     # Evapotranspiration
-    elif ent in [ 'ev24', 'rh', 'rhx' ]:
-        return f'{val}mm'
+    elif e in [ 'ev24', 'rh', 'rhx' ]:
+        return f'{f:.1f}mm'
 
     # Duration hours
-    elif ent in [ 'sq', 'dr' ]:
-        return f'{val} {tr.txt("hour")}'
+    elif e in [ 'sq', 'dr' ]:
+        return f'{f:.0f} {tr.txt("hour")}'
 
     # Wind direction
-    elif ent in  [ 'ddvec' ]:
-        if val == 0:
-            return f'{val} {tr.txt(VAR)}'
+    elif e in  [ 'ddvec' ]:
+        if F == 0.0:
+            return f'{f:.0f}° {tr.txt(VAR)}'
         else:
             # From degrees to direction
             # Source: https://www.campbellsci.com/blog/convert-wind-directions
             ldir = [ 'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S',
                      'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N' ]
-            ndx = int(round(val % 360 / 22.5))
-            return f'{val}° {tr.txt(ldir[ndx])}'
+            ndx = int( round( F % 360 / 22.5 ) )
+            dir = ldir[ndx]
+            return f'{f:.0f}° {tr.txt(dir)}'
 
     # View distance
-    elif ent in [ 'vvn', 'vvx' ]:
-        if val == 0:
+    elif e in [ 'vvn', 'vvx' ]:
+        if f == 0.0:
             return '<100m'
         else:
-            if val < 49:
-                i2 = val + 1
-                return f'{val*100}-{i2*100}m'
-            elif val == 50:
+            if f < 49.0:
+                f1 = f * 100.0
+                f2 = (f + 1.0) * 100.0
+                return f'{f1:.0f}-{f2:.0f}m'
+            elif f == 50.0:
                 return '5-6km'
-            elif val <= 79:
-                i1, i2 = val - 50, val - 49
-                return f'{i1}-{i2}km'
-            elif val <= 89:
-                i1, i2 = val - 50, val - 45
-                return f'{i1}-{i2}km'
+            elif f <= 79.0:
+                f1 = f - 50.0,
+                f2 = f - 49.0
+                return f'{f1:.0f}-{f2:.0f}km'
+            elif f <= 89.0:
+                f1 = f - 50.0,
+                f2 = f - 45.0
+                return f'{f1:.0f}-{f2:.0f}km'
             else:
                 return '>70km'
 
-    return val  # Without string casting will give an error with unknowm data entity
+    return f  # Without string casting will give an error with unknowm data entity
