f41b4175381144a0c3b693f5d3f0136a34adbc05
mzwaving <markzwaving@gmail.com>
2020-07-15 12:18:58 +0200

    Removed unnecessary variable choice
    Updated references for import. Because knmi map is deleted.
    Now sources in knmi map are moved to the main sources map.

--- WeatherStatsNL.py
+++ WeatherStatsNL.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
-'''WeatherStatsNL calculates weather statistics for dutch cities
-from the data from the knmi'''
-
-__author__     =  "Mark Zwaving"
-__email__      =  "markzwaving@gmail.com"
-__copyright__  =  "Copyright 2020 (C) Mark Zwaving. All rights reserved."
-__license__    =  "GNU Lesser General Public License (LGPL)"
-__version__    =  "0.0.9"
-__maintainer__ =  "Mark Zwaving"
-__status__     =  "Development"
-
-import os
+'''WeatherStatsNL calculates weather statistics for dutch cities with data from the knmi'''
+__author__     =  'Mark Zwaving'
+__email__      =  'markzwaving@gmail.com'
+__copyright__  =  'Copyright 2020 (C) Mark Zwaving. All rights reserved.'
+__license__    =  'GNU Lesser General Public License (LGPL)'
+__version__    =  '0.1.0'
+__maintainer__ =  'Mark Zwaving'
+__status__     =  'Development'
 
 # Import libraries
 import config
-import knmi.model.daydata as  daydata
-import knmi.model.stats as stats
-import knmi.control.menu as control_menu
-import knmi.view.fix as fix
 import view.log as log
-import view.menu as view_menu
+import view.menu as menu
 
 # Main programm
-if __name__== "__main__":
+if __name__== '__main__':
     log.header( 'Welcome to WeatherStatsNL', True )
     if config.stations.size == 0:
-        view_menu.error_no_stations_found()
+        menu.error_no_stations_found()
     else:
-        choice = view_menu.main_menu()
+        menu.main_menu()
 
     log.footer('Good bye', True )
