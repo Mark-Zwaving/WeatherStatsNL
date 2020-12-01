2c0401413325bf3e5b03eca7dacf90cc18831f4c
Mark Zwaving <markzwaving@gmail.com>
2020-12-01 22:01:45 +0100

    Create search4days.js
    TODO inits

---
+++ data/html/search-for-days/js/search4days.js
@@ -0,0 +1,34 @@
+/**
+ * Column titles to be sorted for search4days html file
+ * @author   M.Zwaving
+ * @license  MIT-license
+ * @version  0.0.1
+ */
+
+col_titles = {
+    PLACE: obj('PLACE', sort_txt, ascending,  row_nr,  1),
+    DATE:  obj('DATE',  sort_num, descending, row_nr,  3),
+    TX:    obj('TX',    sort_num, ascending,  row_nr,  4),
+    TG:	   obj('TG',    sort_num, ascending,  row_nr,  5),
+    TN:    obj('TN',    sort_num, ascending,  row_nr,  6),
+    T10N:  obj('T10N',  sort_num, ascending,  row_nr,  7),
+    SQ:    obj('SQ',    sort_num, ascending,  row_nr,  8),
+    RH:    obj('RH',    sort_num, ascending,  row_nr,  9),
+    UG:    obj('UG',    sort_num, ascending,  row_nr, 10),
+    NG:    obj('NG',    sort_num, ascending,  row_nr, 11),
+    DDVEC: obj('DDVEC', sort_num, ascending,  row_nr, 12),
+    FHVEC: obj('FHVEC', sort_num, ascending,  row_nr, 13),
+    FG:    obj('FG',    sort_num, ascending,  row_nr, 14),
+    FHX:   obj('FHX',   sort_num, ascending,  row_nr, 15),
+    FHN:   obj('FHN',   sort_num, ascending,  row_nr, 16),
+    FXX:   obj('FXX',   sort_num, ascending,  row_nr, 17),
+    SP:    obj('SP',    sort_num, ascending,  row_nr, 18),
+    Q:     obj('Q',     sort_num, ascending,  row_nr, 19),
+    DR:    obj('DR',    sort_num, ascending,  row_nr, 20),
+    RHX:   obj('RHX',   sort_num, ascending,  row_nr, 21),
+    PG:    obj('PG',    sort_num, ascending,  row_nr, 22),
+    PX:    obj('PX',    sort_num, ascending,  row_nr, 23),
+    PN:    obj('PN',    sort_num, ascending,  row_nr, 24),
+    VVN:   obj('VVN',   sort_num, ascending,  row_nr, 25),
+    VVX:   obj('VVX',   sort_num, ascending,  row_nr, 26)
+}
