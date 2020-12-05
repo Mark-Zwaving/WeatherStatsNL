6ca1c8c8d5e31e4faa52e59ffd523af78bee2159
Mark Zwaving <markzwaving@gmail.com>
2020-12-06 00:16:58 +0100

    Added requests fn for opening txt and json files on the web

--- sources/model/download.py
+++ sources/model/download.py
@@ -4,11 +4,12 @@ __author__     =  "Mark Zwaving"
 __email__      =  "markzwaving@gmail.com"
 __copyright__  =  "Copyright 2020 (C) Mark Zwaving. All rights reserved."
 __license__    =  "GNU Lesser General Public License (LGPL)"
-__version__    =  "0.0.3"
+__version__    =  "0.0.4"
 __maintainer__ =  "Mark Zwaving"
 __status__     =  "Development"
 
 import threading, view.log as log, urllib, model.utils as utils
+import requests, json
 
 # Download file
 def file( url, file ):
@@ -26,7 +27,35 @@ def file( url, file ):
         except Exception as e:
             log.console(f'Download failed.\nError: {e}', True)
         else:
-            log.console(f'Download succes.\n', True)
+            log.console(f'Download succes.', True)
             ok = True
 
     return ok
+
+def request( url, type='txt'):
+    '''Function makes the request based on the url given as parameter
+       The return values are: ok, True if succes else False... And the text From
+       the request.'''
+    ok, t = False, ''
+
+    with threading.Lock():
+        try:
+            resp = urllib.request.urlopen( url )
+            data = resp.read()
+            if type == 'text':
+                t = data
+            elif type == 'json':
+                t = json.loads(data)
+        except Exception as e:
+            log.console(f'Failed request from {url}.\nError: {e}', True)
+        else:
+            log.console(f'Succesfull request from {url}', True)
+            ok = True
+
+    return ok, t
+
+def request_text( url ):
+    return request( url, 'txt')
+
+def request_json( url ):
+    return request( url, 'json')
