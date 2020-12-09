65dcd185af2fef00b0f750bbd956cd8c8c083964
Mark Zwaving <markzwaving@gmail.com>
2020-12-09 07:33:21 +0100

    Added fn: is_yes, is_no, is_quit
    Updated fn is_nan, is_float.
    Added fn is_data_map_empthy eand
    Only exists stations in maplist in the configow data can be reached (for selecting winter or summerstats, if its really there in the data map.
    Not anymore based on the sometime virtual list in the config file

--- sources/model/utils.py
+++ sources/model/utils.py
@@ -8,41 +8,118 @@ __version__    =  '0.0.7'
 __maintainer__ =  'Mark Zwaving'
 __status__     =  'Development'
 
-import config as cfg, datetime, os, re, math, random
+import config, stations, datetime, os, re, math, random, threading
 import http.client as httplib, numpy as np
-import view.translate as tr
-import model.download as download
-import model.read as read
-import view.log as log
 from datetime import datetime
 from dateutil import rrule
 from pytz import timezone
+import sources.view.translate as tr
+import sources.control.fio as fio
+import sources.view.log as log
 
 rnd_digit = lambda min,max  : random.randint(min, max)
 file_path = lambda dir,file : os.path.join(dir, file)
 mk_path   = lambda dir,file : os.path.abspath( file_path(dir, file) )
 
+def s_in_list(s, l):
+    for el in l:
+        if str(s).lower() == el.lower():
+            return True
+    return False
+
+def is_yes(s):
+    ok = s_in_list(s, config.answer_yes)
+    return ok
+
+def is_no(s):
+    ok = s_in_list(s, config.answer_no)
+    return ok
+
+def is_quit(s):
+    ok = s_in_list(s, config.answer_quit)
+    return ok
+
 def var_dump( v ):
     print(id(v), type(v), v)
 
-def is_empthy( answ ):
-    if not answ or answ == '':
-        return True
-    else:
-        return False
-
-def has_internet(url=cfg.check_internet_url, timeout=0.1):
+def is_empthy( val ):
     ok = False
-    connect = httplib.HTTPConnection(url, timeout=timeout)
-    try:
-        connect.request('HEAD', '/')
-        connect.close()
-    except Exception as e:
-        pass
-    else:
+    if not val:
         ok = True
 
     return ok
+
+def is_nan( val ):
+    ok = False
+    try:
+        if val != val:
+            ok = True
+        elif val == np.isnan:
+            ok = True
+        elif np.isnan(val):
+            ok = True
+        elif math.isnan(val):
+            ok = True
+    except Exception as e:
+        pass
+
+    return ok
+
+def is_float( val ):
+    ok = True
+    try:
+        if val is None:
+            ok = False
+        elif is_nan(val):
+            ok = False
+        else:
+            f = float(val)
+    except Exception as e:
+        log.console(f'Digit {val} is no float.')
+        ok = False
+
+    return ok
+
+def has_internet(url=config.check_internet_url, timeout=0.1):
+    ok = False
+    with threading.Lock():
+        connect = httplib.HTTPConnection(url, timeout=timeout)
+        try:
+            connect.request('HEAD', '/')
+            connect.close()
+        except Exception as e:
+            pass
+        else:
+            ok = True
+
+    return ok
+
+def is_data_map_empthy():
+    ok, dir = False, config.dir_data_dayvalues
+    with threading.Lock():
+        if os.path.exists(dir):
+            l = os.listdir(dir)
+            if len(l) == 0:
+                ok = True
+            else:
+                paths = [os.path.join(dir, el) for el in l]
+                for station in stations.list:
+                    if station.data_txt_path in paths:
+                        ok = False
+                        break
+                else:
+                    ok = True
+    return ok
+
+def only_existing_stations_in_map():
+    l, dir = list(), config.dir_data_dayvalues
+    with threading.Lock():
+        if os.path.exists(dir):
+            paths = [os.path.join(dir, el) for el in os.listdir(dir)]
+            for station in stations.list:
+                if station.data_txt_path in paths:
+                    l.append(station)
+    return l
 
 def unique_list(l):
     unique = list()
@@ -104,40 +181,42 @@ def b_utf8_to_s( b ):
 def download_and_read_file(url, file):
     ok, t = False, ''
     if has_internet():
-        ok = download.file( url, file )
+        ok = fio.download( url, file )
         if ok:
-            ok, t = read.file(file)
+            ok, t = fio.read(file)
     else:
         log.console('No internet connection...', True)
 
     return ok, t
 
 def loc_date_now():
-    dt_utc = datetime.now() # UTC propably
-    tz = timezone(cfg.timezone)
-    dt_loc = dt_utc.replace(tzinfo=tz)
+    dt = datetime.now() # UTC ?
+    dt_loc = dt.replace(tzinfo=timezone(config.timezone)) # Now local
 
-    return dt_loc.now()
+    return dt_loc.now() # return local
 
-def isnan( f ):
-    x = float(f)
-    if math.isnan(x) or np.isnan(f):
-        return True
-    else:
-        return False
+def now_for_file():
+    t =  loc_date_now().strftime('%Y%m%d%H%M%S')
+    return t
 
-def mk_name( base='x', period='x', stations=[], entities=[] ):
+def now_created_notification():
+    ds = loc_date_now().strftime('%A, %d %B %Y %H:%M')
+    return config.created_by_notification % ds
+
+def ymd_to_txt( ymd ):
+    ymd = ymd if type(ymd) is str else f_to_s(ymd)
+    return datetime.strptime(ymd, '%Y%m%d').strftime('%A, %d %B %Y')
+
+def mk_name( base='x', period='x', places=[], entities=[] ):
     st = base + '-' + period.replace('*', 'x')
-    input(st)
-    for s in stations:
+    for s in places:
         st += f'-{s.wmo}-'
     for e in entities:
         st += f'-{e}-'
-    input(st)
     return st[:-1]
 
-def mk_name_type( base='x', period='x', stations=[], entities=[], type='txt' ):
-    return mk_name( base, period, stations, entities ) + f'.{type}'
+def mk_name_type( base='x', period='x', places=[], entities=[], type='txt' ):
+    return mk_name( base, period, places, entities ) + f'.{type}'
 
 def f_to_s( f ):
     return str(int(round(f)))
@@ -151,34 +230,28 @@ def clear( s ):
 
 def make_query_txt_only(query):
     q = query.lower()
-    q = q.replace('>=', ' ge ')
-    q = q.replace('≥',  ' ge ')
-    q = q.replace('<=', ' le ')
-    q = q.replace('≤',  ' le ')
-    q = q.replace('==', ' eq ')
-    q = q.replace('!=', ' ne ')
-    q = q.replace('<>', ' ne ')
-    q = q.replace('!=', ' ne ')
-    q = q.replace('>',  ' gt ')
-    q = q.replace('<',  ' lt ')
-    q = q.replace('||', ' or ')
-    q = q.replace('&&', ' and ')
+    q = q.replace('ge',  ' ge ')
+    q = q.replace('le',  ' le ')
+    q = q.replace('eq',  ' eq ')
+    q = q.replace('ne',  ' ne ')
+    q = q.replace('gt',  ' gt ')
+    q = q.replace('lt',  ' lt ')
+    q = q.replace('or',  ' or ')
+    q = q.replace('and', ' and ')
+    q = q.replace('>=',  ' ge ')
+    q = q.replace('≥',   ' ge ')
+    q = q.replace('<=',  ' le ')
+    q = q.replace('≤',   ' le ')
+    q = q.replace('==',  ' eq ')
+    q = q.replace('!=',  ' ne ')
+    q = q.replace('<>',  ' ne ')
+    q = q.replace('!=',  ' ne ')
+    q = q.replace('>',   ' gt ')
+    q = q.replace('<',   ' lt ')
+    q = q.replace('||',  ' or ')
+    q = q.replace('&&',  ' and ')
 
     return clear(q)
-
-def ymd_to_txt( ymd ):
-    ymd = ymd if type(ymd) is str else f_to_s(ymd)
-    return datetime.strptime(ymd, '%Y%m%d').strftime('%A, %d %B %Y')
-
-def now_act_for_file():
-    txt =  datetime.now().strftime('%Y%m%d%H%M%S')
-    return txt
-
-def quit_menu(el):
-    if np.array_equal(el, cfg.answer_quit):
-        return True
-    else:
-        return False
 
 # TODO testing
 def add_zero_less_x(d, x):
