3af0ec6ea2e3bef73968c05c672b950ee76928ef
Mark Zwaving <markzwaving@gmail.com>
2020-12-09 07:30:36 +0100

    Min and max values are now based on sort lists
    Added created by notification in footer
    Added table column for data copyright notice from knmi ie.
    Updated references to modules.

--- sources/model/summerstats.py
+++ sources/model/summerstats.py
@@ -10,14 +10,14 @@ __status__     =  "Development"
 
 import config
 import numpy as np
-import view.log as log
-import view.html as html
-import view.fix as fix
-import view.icon as icon
-import control.io as io
-import model.utils as utils
-import model.stats as stats
-import model.daydata as daydata
+import sources.view.log as log
+import sources.view.html as html
+import sources.view.fix as fix
+import sources.view.icon as icon
+import sources.control.fio as fio
+import sources.model.utils as utils
+import sources.model.stats as stats
+import sources.model.daydata as daydata
 
 class Stats:
     '''Class saves en stores summer statistics of a station in a given period'''
@@ -30,17 +30,17 @@ class Stats:
         self.period  = f'{self.date_s}-{self.date_e}'
 
         self.tg_gem = stats.average(data,'TG') # Avergae TG
-        self.tx_max = stats.max(data,'TX') # Highest TX
         self.tx_max_sort = stats.sort( data, 'TX')
-        self.tg_max = stats.max(data,'TG') # Highest TG
+        self.tx_max      = self.tx_max_sort[0, daydata.ndx_ent('TX')] # Get max is 1st in list
         self.tg_max_sort = stats.sort( data, 'TG')
-        self.tn_max = stats.max(data,'TN') # Highest TN
+        self.tg_max      = self.tg_max_sort[0, daydata.ndx_ent('TG')]
         self.tn_max_sort = stats.sort( data, 'TN')
+        self.tn_max      = self.tn_max_sort[0, daydata.ndx_ent('TN')]
 
         self.sq_tot = stats.sum(data,'SQ') # Total sunshine hours
-        self.sq_tot_sort = stats.sort( data, 'SQ')
+        self.sq_sort = stats.sort(data, 'SQ')
         self.rh_tot = stats.sum(data,'RH') # Total rain
-        self.rh_tot_sort = stats.sort( data, 'RH')
+        self.rh_sort = stats.sort( data, 'RH')
 
         self.days_tx_gte_20 = stats.terms_days(data,'TX','≥',20) # Warm days
         self.days_tx_gte_25 = stats.terms_days(data,'TX','≥',25) # Summer days
@@ -57,7 +57,7 @@ class Stats:
         self.days_heat_ndx = self.days_tg_gte_18
 
 def sort( l, pm = '+' ):
-    l = np.array( sorted(l, key=lambda stats: stats.heat_ndx) ) # Sort on heat index
+    l = np.array( sorted(np.array(l), key=lambda stats: stats.heat_ndx) ).tolist() # Sort on heat index
     if pm == '+':
         l = l[::-1] # reverse
 
@@ -65,17 +65,17 @@ def sort( l, pm = '+' ):
 
 def calculate( stations, period, name=False, type='html' ):
     '''Function calculates summer statistics'''
-    colspan = 19
+    colspan = 20
 
     # Make data list with station and summerstatistics
-    summer = np.array( [] )
+    summer = list()
     for station in stations:
         log.console(f'Calculate statistics: {station.place}', True)
         ok, data = daydata.read( station )  # Get data stations
         if ok:
             days = daydata.period( data, period ) # Get days of period
             if days.size != 0: # Skip station
-                summer = np.append( summer, Stats( station, days ) ) # Create summerstats object
+                summer.append( Stats( station, days ) ) # Create summerstats object
 
     log.console(f'\nPreparing output: {type}', True)
 
@@ -138,6 +138,7 @@ def calculate( stations, period, name=False, type='html' ):
                     </th>
                 </tr>
                 <tr>
+                    <th title="copyright data_notification"> </th>
                     <th> place {icon.home(size='fa-sm')}</th>
                     <th> province {icon.flag(size='fa-sm')}</th>
                     <th> period {icon.cal_period(size='fa-sm')}</th>
@@ -197,7 +198,9 @@ def calculate( stations, period, name=False, type='html' ):
         if type == 'html':
             period_txt = f'{utils.ymd_to_txt(s.date_s)} - {utils.ymd_to_txt(s.date_e)}'
             main += f'''
-                <tr>
+                    <td title="{s.station.data_notification.lower()}">
+                            {icon.copy_light(size='fa-xs')}
+                    </td>
                     <td> <span class="val"> {s.station.place} </span> </td>
                     <td> <span class="val"> {s.station.province} </span> </td>
                     <td title="{period_txt}"> <span class="val"> {s.period} </span> </td>
@@ -248,7 +251,7 @@ def calculate( stations, period, name=False, type='html' ):
                     </td>
                     <td>
                         <span class="val"> {sq_tot} </span>
-                        {html.table_days( s.sq_tot_sort, 'SQ' )}
+                        {html.table_days( s.sq_sort, 'SQ' )}
                     </td>
                     <td>
                         <span class="val"> {np.size(s.days_sq_gte_10, axis=0)} </span>
@@ -256,7 +259,7 @@ def calculate( stations, period, name=False, type='html' ):
                     </td>
                     <td>
                         <span class="val"> {rh_tot} </span>
-                        {html.table_days( s.rh_tot_sort, 'RH' )}
+                        {html.table_days( s.rh_sort, 'RH' )}
                     </td>
                     <td>
                         <span class="val"> {np.size(s.days_rh_gte_10, axis=0)} </span>
@@ -266,7 +269,7 @@ def calculate( stations, period, name=False, type='html' ):
                 '''
 
     if type in ['txt','cmd']:
-        footer += config.knmi_dayvalues_notification
+        footer += stations[0].data_notification
 
     if type == 'html':
         footer += f'''
@@ -274,7 +277,7 @@ def calculate( stations, period, name=False, type='html' ):
             <tfoot>
                 <tr>
                     <td colspan="{colspan}">
-                        {config.knmi_dayvalues_notification}
+                        {utils.now_created_notification()}
                     </td>
                 </tr>
             </tfoot>
@@ -304,6 +307,6 @@ def calculate( stations, period, name=False, type='html' ):
         page.save()
 
     elif type == 'txt':
-        io.save(path, output) # Schrijf naar bestand
+        fio.save(path, output) # Schrijf naar bestand
 
     return path
--- sources/model/winterstats.py
+++ sources/model/winterstats.py
@@ -10,14 +10,14 @@ __status__     =  'Development'
 
 import config
 import numpy as np
-import view.log as log
-import view.html as html
-import view.fix as fix
-import view.icon as icon
-import control.io as io
-import model.utils as utils
-import model.stats as stats
-import model.daydata as daydata
+import sources.view.log as log
+import sources.view.html as html
+import sources.view.fix as fix
+import sources.view.icon as icon
+import sources.control.fio as fio
+import sources.model.utils as utils
+import sources.model.stats as stats
+import sources.model.daydata as daydata
 
 class Stats:
     '''Class stores and saves winter statistics of a station in a given period'''
@@ -33,17 +33,17 @@ class Stats:
         self.tg_gem = stats.average( data, 'TG' )
 
         # Min extremes
-        self.tx_min = stats.min( data, 'TX' )
         self.tx_min_sort = stats.sort( data, 'TX', reverse=True )
-        self.tg_min = stats.min( data, 'TG' )
+        self.tx_min      = self.tx_min_sort[0, daydata.ndx_ent('TX')]
         self.tg_min_sort = stats.sort( data, 'TG', reverse=True )
-        self.tn_min = stats.min( data, 'TN' )
+        self.tg_min      = self.tg_min_sort[0, daydata.ndx_ent('TG')]
         self.tn_min_sort = stats.sort( data, 'TN', reverse=True )
+        self.tn_min      = self.tn_min_sort[0, daydata.ndx_ent('TN')]
 
-        self.rh_sum = stats.sum( data, 'RH' )
-        self.rh_sum_sort = stats.sort( data, 'RH' )
-        self.sq_sum = stats.sum( data, 'SQ' )
-        self.sq_sum_sort = stats.sort( data, 'SQ' )
+        self.rh_sort = stats.sort( data, 'RH' )
+        self.rh_sum  = stats.sum( data, 'RH' )
+        self.sq_sort = stats.sort( data, 'SQ' )
+        self.sq_sum  = stats.sum( data, 'SQ' )
 
         # Days lists
         self.days_tx_lt_0   = stats.terms_days( data, 'TX', '<',   0 )
@@ -60,25 +60,26 @@ class Stats:
         self.ijnsen         = stats.ijnsen( data )
 
 def sort( l, ent, pm = '+' ):
-    l = np.array( sorted(l, key=lambda stats: stats.sum_hellmann) ) # Sort on hellmann
-    if pm == '+': l = l[::-1] # reverse
+    l = np.array( sorted(np.array(l), key=lambda stats: stats.sum_hellmann) ).tolist() # Sort on hellmann
+    if pm == '+':
+        l = l[::-1] # reverse
 
     return l
 
 def calculate( stations, period, name, type='html' ):
     '''Function to calculate winterstatistics'''
     log.console(f'Preparing output...')
-    colspan = 19
+    colspan = 20
 
     # Make data list with station and stats
-    winter = np.array( [] )
+    winter = list()
     for station in stations:
         log.console(f'Calculate statistics: {station.place}', True)
         ok, data = daydata.read( station )  # Get data stations
         if ok:
             days = daydata.period( data, period ) # Get days of period
             if days.size != 0:
-                winter = np.append( winter, Stats( station, days ) ) # Create winterstats object
+                winter.append( Stats( station, days ) ) # Create winterstats object
 
     log.console(f'\nPreparing output: {type}', True)
 
@@ -127,6 +128,7 @@ def calculate( stations, period, name, type='html' ):
                     </th>
                   </tr>
                   <tr>
+                    <th title="copyright data_notification"> &nbsp; </th>
                     <th> place {icon.home(size='fa-sm')}</th>
                     <th> province {icon.flag(size='fa-sm')}</th>
                     <th> period {icon.cal_period(size='fa-sm')}</th>
@@ -169,6 +171,9 @@ def calculate( stations, period, name, type='html' ):
             # TODO the extension html tables
             main += f'''
                 <tr class="row-data">
+                    <td title="{s.station.data_notification.lower()}">
+                            {icon.copy_light(size='fa-xs')}
+                    </td>
                     <td> <span class="val"> {s.station.place} </span> </td>
                     <td> <span class="val"> {s.station.province} </span> </td>
                     <td title="{period_txt}"> <span class="val"> {s.period} </span> </td>
@@ -195,12 +200,12 @@ def calculate( stations, period, name, type='html' ):
                     </td>
                     <td>
                         <span class="val"> {sq_sum} </span>
-                        {html.table_days( s.sq_sum_sort, 'SQ' )}
+                        {html.table_days( s.sq_sort, 'SQ' )}
 
                     </td>
                     <td>
                         <span class="val"> {rh_sum} </span>
-                        {html.table_days( s.rh_sum_sort, 'RH' )}
+                        {html.table_days( s.rh_sort, 'RH' )}
 
                     </td>
                     <td>
@@ -261,7 +266,7 @@ def calculate( stations, period, name, type='html' ):
             <tfoot>
                 <tr>
                     <td colspan="{colspan}">
-                        {config.knmi_dayvalues_notification}
+                        {utils.now_created_notification()}
                     </td>
                 </tr>
             </tfoot>
@@ -290,6 +295,6 @@ def calculate( stations, period, name, type='html' ):
         page.save()
 
     elif type == 'txt':
-        io.save(path, output) # Schrijf naar bestand
+        fio.save(path, output) # Schrijf naar bestand
 
     return path
